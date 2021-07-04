a18d9d2f6d46613271621b076c3bc5a17610f449
김태형 <thk12378905@gmail.com>
2021-07-04 06:22:35 +0000

    tri tri tir

---
+++ information.js
@@ -0,0 +1,12 @@
+    var arr = [32][]; 
+
+
+    function create2DArray(columns) {
+        var arr = new Array(32);
+        for (var i = 0; i < 32; i++) {
+            arr[i] = new Array(columns);
+        }
+        return arr;
+    }
+
+    fucntion 
